c812523e57a1201db91be2fa15c4a1f45b9a1ab1
Liam Perry <LiamP413@gmail.com>
2017-02-03 12:14:48 -0500

    Music Player Start

--- mobile/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
+++ mobile/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
@@ -3,20 +3,14 @@ package com.github.pocmo.sensordashboard;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 //import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,35 +23,59 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.squareup.otto.Subscribe;
 
+import android.os.IBinder;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+import com.github.pocmo.sensordashboard.MusicService.MusicBinder;
+
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
+
+    //wear vars
     private RemoteSensorManager remoteSensorManager;
-
     Toolbar mToolbar;
-
-//    private ViewPager pager;
+    //private ViewPager pager;
     private View emptyState;
     private NavigationView mNavigationView;
     private Menu mNavigationViewMenu;
     private List<Node> mNodes;
     private float curHeartRate;
     private float curStepRate;
+
+
+    //music player vars
+    private ArrayList<Song> songList;
+    private ListView songView;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound=false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
-        emptyState = findViewById(R.id.empty_state);
+        //mToolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        //emptyState = findViewById(R.id.empty_state);
 
-        mNavigationView = (NavigationView) findViewById(R.id.navView);
-        mNavigationView.setNavigationItemSelectedListener(this);
-        mNavigationViewMenu = mNavigationView.getMenu();
+        //mNavigationView = (NavigationView) findViewById(R.id.navView);
+        //mNavigationView.setNavigationItemSelectedListener(this);
+        //mNavigationViewMenu = mNavigationView.getMenu();
 
-        initToolbar();
+        //initToolbar();
 //        initViewPager();
 
 
@@ -101,7 +119,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //            }
 //        });
 
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+
+
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+        getSongList();
+
+        //sort
+        Collections.sort(songList, new Comparator<Song>(){
+            public int compare(Song a, Song b){
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
+
+
     }
 
 
@@ -304,5 +339,107 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //
 //
 //    }
+    @Override
+    protected void onDestroy()
+    {
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            //case R.id.action_shuffle:
+            //    //shuffle
+            //    break;
+            case R.id.action_end:
+                stopService(playIntent);
+                musicSrv=null;
+                System.exit(0);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void songPicked(View view)
+    {
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+    }
+
+    public void getSongList()
+    {
+        //get the music
+
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst())
+        {
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do
+            {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                if(curHeartRate>=80)
+                {
+                    if(thisTitle.charAt(0)<'O')
+                        songList.add(new Song(thisId, thisTitle, thisArtist));
+
+                }
+                else
+                {
+                    if(thisTitle.charAt(0)>='O')
+                        songList.add(new Song(thisId, thisTitle, thisArtist));
+                }
+            }
+            while (musicCursor.moveToNext());
+        }
+    }
+
+    //connect to the service
+    private ServiceConnection musicConnection = new ServiceConnection()
+    {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            MusicBinder binder = (MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            musicBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
 }
 
